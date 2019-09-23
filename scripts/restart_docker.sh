ec15c7e5ffcf6b3f6777f6faeb8a4702eb7cca88
Hao Li <hayatelee@gmail.com>
2019-09-23 12:32:21 +0800

    1. fix docker cp for symlink 2. open 443 port

--- scripts/restart_docker.sh
+++ scripts/restart_docker.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-docker cp "${RENEWED_LINEAGE}/cert.pem" softethervpn:/usr/vpnserver/new.crt
-docker cp "${RENEWED_LINEAGE}/privkey.pem" softethervpn:/usr/vpnserver/new.key
+docker cp -L "${RENEWED_LINEAGE}/cert.pem" softethervpn:/usr/vpnserver/new.crt
+docker cp -L "${RENEWED_LINEAGE}/privkey.pem" softethervpn:/usr/vpnserver/new.key
 
 docker restart softethervpn
 

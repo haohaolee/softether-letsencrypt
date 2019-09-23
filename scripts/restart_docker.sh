99a8d30919ba1ebc2626581e3e41651d45cc90d2
Hao Li <hayatelee@gmail.com>
2019-09-23 12:35:17 +0800

    add logging

--- scripts/restart_docker.sh
+++ scripts/restart_docker.sh
@@ -5,5 +5,6 @@ set -x
 docker cp -L "${RENEWED_LINEAGE}/cert.pem" softethervpn:/usr/vpnserver/new.crt
 docker cp -L "${RENEWED_LINEAGE}/privkey.pem" softethervpn:/usr/vpnserver/new.key
 
+echo "restarting vpn...."
 docker restart softethervpn
 

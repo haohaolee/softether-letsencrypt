35b6796221d589f38f76708893519a8102a690cc
Hao Li <hayatelee@gmail.com>
2019-09-23 12:50:43 +0800

    ignore error handling

--- scripts/restart_docker.sh
+++ scripts/restart_docker.sh
@@ -6,5 +6,5 @@ docker cp -L "${RENEWED_LINEAGE}/cert.pem" softethervpn:/usr/vpnserver/new.crt
 docker cp -L "${RENEWED_LINEAGE}/privkey.pem" softethervpn:/usr/vpnserver/new.key
 
 echo "restarting vpn...."
-docker restart softethervpn
+docker restart softethervpn 2> /dev/null
 
